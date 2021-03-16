60745d3262674072b3d64a4abdc3da6a964374be
Jakub Biel <jakubbiel97@gmail.com>
2021-03-16 13:15:56 +0100

    SolverInt

---
+++ src/kuba/interview/domino/DominoSolverInt.java
@@ -0,0 +1,9 @@
+package kuba.interview.domino;
+
+public interface DominoSolverInt {
+    String solve(String input, int iterations);
+    String solve(String input);
+
+    String reverseSolving(String input, int iterations);
+    String reverseSolving(String input);
+}
