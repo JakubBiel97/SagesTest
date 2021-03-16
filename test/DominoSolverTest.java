cc57d2500fa99ffcdd9a7154675bd1c3b9686f71
Jakub Biel <jakubbiel97@gmail.com>
2021-03-16 13:34:13 +0100

    Test
    Creating test file

---
+++ test/DominoSolverTest.java
@@ -0,0 +1,12 @@
+import kuba.interview.domino.DominoSolver;
+import kuba.interview.domino.DominoSolverInt;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DominoSolverTest {
+
+   
+}
